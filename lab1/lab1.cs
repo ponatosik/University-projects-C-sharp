f6cc7c917e645d8cecc95fea6cdbd24fbb2822a2
ponatosik <ponatosik@gmail.com>
2022-09-17 21:49:30 +0300

    Add lab1 and lab2

---
+++ lab1/lab1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double U = 0, I = 0, R;
+            bool isDataValid = false;
+            string UStr, IStr;
+
+            while(!isDataValid)
+            {
+                Console.Write("Введіть силу струму I:");
+                UStr = Console.ReadLine();
+
+                Console.Write("Введіть напругу U:");
+                IStr = Console.ReadLine();
+
+                isDataValid =   double.TryParse(UStr, out U) && 
+                                double.TryParse(IStr, out I);
+                
+                if(!isDataValid)
+                    Console.WriteLine("Невірний формат даних, спробйте ще раз");
+            }
+
+            R = I / U;
+            Console.WriteLine($"Опір = {R}");
+        }
+    }
+}
---
+++ lab2/lab2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn = 0, nk = 0;
+            string nnStr, nkStr;
+            bool isDataValid = false;
+
+            while(!isDataValid)
+            {
+                Console.Write("Введіть нижню межу обчислення:");
+                nnStr = Console.ReadLine();
+
+                Console.Write("Введіть верхню межу обчислення:");
+                nkStr = Console.ReadLine();
+
+                isDataValid =   int.TryParse(nnStr, out nn) && 
+                                int.TryParse(nkStr, out nk) && 
+                                nk >= nn;
+                
+                if(!isDataValid)
+                    Console.WriteLine("Невірний формат даних, спробйте ще раз");
+            }
+
+            Func<int, double> a_k = k => (double)
+                (Math.Pow(k, 2) + Math.Pow(-1, k) * Math.Pow(k, 3)) / (Math.Pow(k, 2) + k + 1);
+
+            double sum = Enumerable .Range(nn, nk - nn + 1)
+                                    .Select(k => a_k(k))
+                                    .Sum();
+                                    
+            Console.WriteLine($"Сума членів ряду з {nn} до {nk} = {sum}");
+        }
+    }
+}
